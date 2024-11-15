0152027d9f94335e0245fede8051ba0a58eee98e
Yamini Yadav <111552977+yamini1183@users.noreply.github.com>
2024-11-15 21:55:10 +0530

    Update Api.js

--- frontend/src/components/Api.js
+++ frontend/src/components/Api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const Api=axios.create({
     // baseURL: "http://localhost:8000", //for localhost connection with FastAPI backend 
     // baseURL:"https://aiplanet-fullstack-genrative-ai-app.onrender.com"   //connection with deployed backend deployed FastAPI app 
-    baseURL: "https://shivakerur99-pdf-chatgpt.hf.space"
+    baseURL: "https://pdfdetails.onrender.com/"
 })
 
 export default Api
